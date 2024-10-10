8f0b0a47b50779b20d96e1456348d5b9a327b89e
oxemz <jointstory01@gmail.com>
2024-10-10 08:43:28 +0700

    menambahkan loading.tsx page utama selesai dan data ts updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,8 +6,8 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "@/app/lib/data
 
 export default async function Page() {
   const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+  const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
+  const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } = await fetchCardData(); // wait for fetchLatestInvoices() to finish
 
   return (
     <main>
